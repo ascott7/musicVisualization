fcfb4a972813589dc7a99c850dc3869008cf4542
Andrew Scott <ascott@hmc.edu>
2015-11-26 18:07:32 -0800

    accidentally committed executable instead of cpp file lol

--- software/fft_test2.cpp
+++ software/fft_test2.cpp
@@ -19,15 +19,17 @@ int main(void)
 {
         wav_reader reader("space_oddity.wav");
         chrono::microseconds start = chrono::microseconds(100);
-        chrono::microseconds length = chrono::microseconds(1000);
+        chrono::microseconds length = chrono::microseconds(800);
         vector<float> samples = reader.get_range(start, length);
         
+
         vector<complex<double>> data, copy;
         size_t i;
         int ret;
 
-        for (i = 0; i < samples.size(); ++i)
+        for (i = 0; i < samples.size(); ++i) {
                 data.push_back(complex<double>(samples[i]));
+        }
 
         copy = data;
         ret = fft(copy);
